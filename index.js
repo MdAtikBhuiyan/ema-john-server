134ae01167e6ebb591bcea2dabf121ee4a632844
MdAtikBhuiyan <touhidatik81@gmail.com>
2021-03-28 23:52:56 +0600

    ok

--- index.js
+++ index.js
@@ -117,12 +117,20 @@ client.connect(err => {
         })
     })
 
-    // app.get('/products', (req, res) => {
-    //     productsCollection.find({})
-    //     .toArray((err, documents) =>{
-    //         res.send(documents);
-    //     })
-    // })
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+        .toArray((err, documents) =>{
+            if(!err){
+                res.send(documents);
+            }
+            else{
+               res.json({
+                   message:'something went wrong'
+               })
+            }
+            
+        })
+    })
 
     app.get('/product/:key', (req, res) => {
         productsCollection.find({key: req.params.key})
